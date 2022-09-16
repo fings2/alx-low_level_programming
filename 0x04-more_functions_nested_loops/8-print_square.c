fb38d18e861adff3d9a84692468bcd3cb2d1911f
Ani Raphael <aniraphael09@gmail.com>
2022-09-16 14:02:20 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,6 +6,7 @@
 *@size: parameter
 *Return: returns nothing
 */
+
 void print_square(int size)
 {
 	int inc1, inc2;
@@ -14,7 +15,7 @@ void print_square(int size)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
-			for (inc2 = 0: inc2 < (size - 1); inc2++)
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
 				putchar('#');
 			}
