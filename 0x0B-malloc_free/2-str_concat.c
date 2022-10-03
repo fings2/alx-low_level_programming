236a09fddec9dc22a5350e8e2a6312ce92e7359a
Ani Raphael <aniraphael09@gmail.com>
2022-10-03 16:31:11 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
-	while (s2[c1] != '\0')
+	while (s2[ci] != '\0')
 		ci++;
 	concat = malloc(sizeof(char) * (i + ci + i));
 
